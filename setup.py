ba169abd0f283c96c277feefdca71495857d1d1f
Nikhil Gannu <nikhilgannu@sosonweb.com>
2024-12-23 12:22:50 -0500

    folder Struture added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name = 'Medibot GenAi project',
+    version= '0.0.0',
+    author = 'Nikhil Gannu',
+    author_email='nikhil@gmail.com',
+    packages= find_packages(),
+    install_requires=[]
+)
